2ade898044506c540ec73c519c3df1227058b3f4
RafaMadera18 <rafa_madlop@hotmail.com>
2026-09-04 18:02:08 -0600

    feat: add social media icons for LinkedIn, TikTok, Twitch, YouTube, and X
    - Added SVG icons for LinkedIn, TikTok, Twitch, YouTube, and X to the public/social directory.
    - Created a new CSS file for Tailwind CSS setup.
    - Implemented AuthLayout component for authentication pages.
    - Set up main entry point for React application with routing.
    - Created router configuration for login and registration views.
    - Developed LoginView and RegisterView components with navigation links.
    - Configured Tailwind CSS in the project.
    - Added TypeScript configuration files for app and node environments.
    - Set up Vite configuration for React application.

---
+++ src/views/LoginView.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function LoginView() {
+  return (
+    <>
+      <nav>
+        <Link to="/auth/register">¿No tienes cuenta? Crea una aqui</Link>
+      </nav>
+    </>
+  );
+}
